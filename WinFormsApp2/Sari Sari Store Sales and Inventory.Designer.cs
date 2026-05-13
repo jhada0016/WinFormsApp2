a969aadae7052a145afb94804ab2e0c2f329773d
KEI <KEI@DESKTOP-87OVOUR>
2026-05-13 14:45:51 +0800

    kkkkkkk

---
+++ WinFormsApp2/Sari Sari Store Sales and Inventory.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WinFormsApp2
+{
+    partial class Sari_Sari_Store_Sales_and_Inventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.DarkBlue;
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(3, 1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(153, 449);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Transparent;
+            panel2.Location = new Point(154, 1);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(646, 449);
+            panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 8.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(26, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(101, 13);
+            label1.TabIndex = 0;
+            label1.Text = "SARI - SARI STORE";
+            label1.Click += label1_Click;
+            // 
+            // Sari_Sari_Store_Sales_and_Inventory
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Sari_Sari_Store_Sales_and_Inventory";
+            Text = "Sari_Sari_Store_Sales_and_Inventory";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Panel panel2;
+    }
+}
---
+++ WinFormsApp2/Sari Sari Store Sales and Inventory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Sari_Sari_Store_Sales_and_Inventory : Form
+    {
+        public Sari_Sari_Store_Sales_and_Inventory()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
